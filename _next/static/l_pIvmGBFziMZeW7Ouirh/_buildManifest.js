d4305130da2a889f59a90189bb6e9031fa238d08
Carlos Salas <josesalasni@gmail.com>
2022-05-01 19:38:20 -0600

    Updates

---
+++ _next/static/l_pIvmGBFziMZeW7Ouirh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/393332bdadad5ff4.css","static/chunks/pages/index-56c95e568197b4a6.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
