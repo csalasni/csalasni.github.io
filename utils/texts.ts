dcd696182506d4b7288ffc7985d97e38433b91d7
Carlos Salas <josesalasni@gmail.com>
2022-07-21 16:19:39 -0600

    feat: update header

--- utils/texts.ts
+++ utils/texts.ts
@@ -2,7 +2,7 @@ const Texts = {
   title: 'Carlos Salas',
   subtitle: `I'm a Software Developer.`,
   introduction: 'Hi, my name is',
-  headDescription: `I'm a Software Developer from Nicaragua, interested in the Web Technologies and excited to contribute to the programming community.`,
+  headDescription: `I'm a Software Developer that have been working in the design and analysis of Rest / GraphQL Api's, and microservices that consume SQL Databases / third-party apis.`,
   description: `I have been working in the design and analysis of Rest / GraphQL Api's, and microservices that consume SQL Databases / third-party apis alongside the design of user interfaces using React.Js. `,
   projects: 'Personal Projects',
 }
