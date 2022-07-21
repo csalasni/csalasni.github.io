0b6279fedd48cdce23d9ff637a6c58059757bedb
salascarlosni <salascarlosni@users.noreply.github.com>
2022-07-21 08:15:49 +0000

    deploy: fccef77f69f0706929daaf2a43f61911dc4bb188

--- _next/static/RG8J7hhxNtO_QQSW8VTYz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c6f840cbf97374ad.css","static/chunks/pages/index-df3f9e3cdb62a928.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
