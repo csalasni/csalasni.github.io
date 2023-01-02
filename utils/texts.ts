8d0a91d0732b636694bfc57a0181c4d22d2be312
Carlos Salas <salascarlosni@gmail.com>
2023-01-02 15:47:13 -0600

    feat: updating job title

--- utils/texts.ts
+++ utils/texts.ts
@@ -1,8 +1,8 @@
 const Texts = {
   title: 'Carlos Salas',
-  subtitle: `I'm a Full Stack Developer.`,
+  subtitle: `I'm a Software Engineer.`,
   introduction: 'Hi, my name is',
-  headDescription: `I'm a Full Stack Developer, I have been working in the design and analysis of Rest / GraphQL Api's, and microservices that consume SQL Databases / third-party apis.`,
+  headDescription: `I'm a Software Engineer, I have been working in the design and analysis of Rest / GraphQL Api's, and microservices that consume SQL Databases / third-party apis.`,
   description: `I have been working in the design and analysis of Rest / GraphQL Api's, and microservices that consume SQL Databases / third-party apis alongside the design of user interfaces using React.Js. `,
   projects: 'Projects',
   techs: 'Technologies',
