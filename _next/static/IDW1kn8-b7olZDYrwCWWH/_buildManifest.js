19d55f088520f5a450b9e0ab0bc3f4aab0be8c57
csalasni <csalasni@users.noreply.github.com>
2025-11-24 22:37:04 +0000

    deploy: 858cfd7e4381d0752d1312244870f033905fde13

---
+++ _next/static/IDW1kn8-b7olZDYrwCWWH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/71ed1326cd8b1f98.css","static/chunks/pages/index-0374d09c0b7bc44f.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
