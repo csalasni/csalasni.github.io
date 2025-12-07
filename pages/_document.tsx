adaa8332ed20c088aa64651f887e8a7f293a360b
Carlos Salas <salascarlosni@gmail.com>
2025-12-06 22:38:37 -0600

    feat: hosting fonts locally to improve load speed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,16 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300..800&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <body>
         <Main />
